a82f54f03ae6e97fbd9357b2de5f2521fd92b286
Ankit505818 <sharmaankit2690@gmail.com>
2025-06-02 15:36:12 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-adobe-express---file-1",
+      "name": "Adobe Express - file (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.08219258719199019,
+        "pitch": 0.14634646890380054,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3583655273613271,
+          "pitch": -0.05827276256897207,
+          "rotation": 0.7853981633974483,
+          "target": "1-adobe-express---file-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-adobe-express---file-2",
+      "name": "Adobe Express - file (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
